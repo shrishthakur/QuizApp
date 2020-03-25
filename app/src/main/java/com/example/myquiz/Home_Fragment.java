611af0b9d943d069a63ba40f71eef1178428d76b
shrishthakur <39955772+shrishthakur@users.noreply.github.com>
2020-03-25 20:45:16 +0530

    quiz app so far 23/03

--- app/src/main/java/com/example/myquiz/Home_Fragment.java
+++ app/src/main/java/com/example/myquiz/Home_Fragment.java
@@ -3,11 +3,16 @@ package com.example.myquiz;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,7 +20,12 @@ import android.widget.TextView;
  */
 public class Home_Fragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
     private TextView tv;
+    private HomeAdapter homeAdapter;
+    private  RecyclerView.LayoutManager layoutManager;
+    private List<HomeViewModel> homelist= new ArrayList<>();//without new it was giving null pointer exception
 
     public Home_Fragment() {
         // Required empty public constructor
@@ -27,8 +37,33 @@ public class Home_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view =inflater.inflate(R.layout.fragment_topics_, container, false);
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
         tv = view.findViewById(R.id.tt);
+        recyclerView = view.findViewById(R.id.rv);
+        layoutManager = new GridLayoutManager(getContext(),1);
+        recyclerView.setHasFixedSize(true);//
+        recyclerView.setLayoutManager(layoutManager);
+        homeAdapter = new HomeAdapter(getContext(),homelist);
+
+        recyclerView.setAdapter(homeAdapter);
+        prepareHomeData();
         return view;
     }
+
+    private void prepareHomeData()
+    {
+        int[] images={R.drawable.download6,R.drawable.download2,R.drawable.download3,
+                R.drawable.download4,R.drawable.download5
+        };
+
+       HomeViewModel a = new HomeViewModel(images[0],"Fact1");
+       homelist.add(a);
+       homelist.add(new HomeViewModel(images[1],"Fact2"));
+        homelist.add(new HomeViewModel(images[2],"Fact3"));
+        homelist.add(new HomeViewModel(images[3],"Fact4"));
+        homelist.add(new HomeViewModel(images[4],"Fact5"));
+    }
 }
+
+
+
